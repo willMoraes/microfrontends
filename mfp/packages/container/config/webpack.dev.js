28d15053e6ea4824e88c992f3b2515a70b34d929
willMoraes <willmoraes.ev@gmail.com>
2023-01-29 22:30:34 -0300

    feat: :sparkles: add package dependencies as shared dependencies from module federation

--- mfp/packages/container/config/webpack.dev.js
+++ mfp/packages/container/config/webpack.dev.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge');
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const commonConfig = require('./webpack.common')
+const packageJson = require('../package.json')
 
 const devConfig = {
   mode: 'development',
@@ -16,7 +17,8 @@ const devConfig = {
       name: 'container',
       remotes: {
         marketing: 'marketing@http://localhost:8081/remoteEntry.js'
-      }
+      },
+      shared: packageJson.dependencies
     }),
     new HtmlWebPackPlugin({
       template: './public/index.html'
--- mfp/packages/marketing/config/webpack.dev.js
+++ mfp/packages/marketing/config/webpack.dev.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge');
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const commonConfig = require('./webpack.common')
+const packageJson = require('../package.json')
 
 const devConfig = {
   mode: 'development',
@@ -17,7 +18,8 @@ const devConfig = {
       filename: 'remoteEntry.js',
       exposes: {
         './MarketingApp' : './src/bootstrap'
-      }
+      },
+      shared: packageJson.dependencies
     }),
     new HtmlWebPackPlugin({
       template: './public/index.html'
