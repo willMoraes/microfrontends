86126f93f374bd327aad36d252724fe0045c7deb
willMoraes <willmoraes.ev@gmail.com>
2023-01-30 20:33:24 -0300

    feat: :sparkles: add webpack prod config to container and marketing

--- mfp/packages/container/config/webpack.dev.js
+++ mfp/packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HtmlWebPackPlugin = require('html-webpack-plugin')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const commonConfig = require('./webpack.common')
 const packageJson = require('../package.json')
@@ -20,9 +19,7 @@ const devConfig = {
       },
       shared: packageJson.dependencies
     }),
-    new HtmlWebPackPlugin({
-      template: './public/index.html'
-    })
+
   ]
 };
 
