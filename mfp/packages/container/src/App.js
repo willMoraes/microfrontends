41f72f96ba5e53e92039afc1dea5604898bacb68
willMoraes <willmoraes.ev@gmail.com>
2023-02-26 19:37:46 -0300

    feat: :sparkles: add auth control to container and children

--- mfp/packages/container/src/App.js
+++ mfp/packages/container/src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import MarketingApp from './components/MarketingApp';
-import AuthApp from './components/AuthApp';
-import Header from './components/Header';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
+import Header from './components/Header';
+import Progress from './components/progress';
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
 })
 
+const MarketingLazy = lazy(() => import('./components/MarketingApp'))
+const AuthLazy = lazy(() => import('./components/AuthApp'))
+
 export default () => {
+  const [isSignIn, setIsSignIn] = useState(false)
+
   return (
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
           <div>
-            <Header />
-            <Switch>
-              <Route path="/auth" component={AuthApp} />
-              <Route path="/" component={MarketingApp} />
-            </Switch>
-            <MarketingApp />
+            <Header isSignIn={isSignIn} onSignOut={() => setIsSignIn(false)}/>
+            <Suspense fallback={<Progress />}>
+              <Switch>
+                <Route path="/auth" component={AuthLazy}> 
+                  <AuthLazy onSignIn={() => setIsSignIn(true)}/>
+                </Route>
+                <Route path="/" component={MarketingLazy} />
+              </Switch>
+            </Suspense>
           </div>
       </StylesProvider>
     </BrowserRouter>
