903fc7250667356bf6e29c6bae90d0c00c76d6f9
willMoraes <willmoraes.ev@gmail.com>
2023-02-26 21:33:28 -0300

    feat: :sparkles: add dashboard mfe with component, route and workflow

--- mfp/packages/container/src/App.js
+++ mfp/packages/container/src/App.js
@@ -1,21 +1,31 @@
-import React, { lazy, Suspense, useState } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { lazy, Suspense, useState, useEffect } from 'react';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 import Header from './components/Header';
 import Progress from './components/progress';
+import { createBrowserHistory } from 'history'
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
 })
 
+const history = createBrowserHistory()
+
 const MarketingLazy = lazy(() => import('./components/MarketingApp'))
 const AuthLazy = lazy(() => import('./components/AuthApp'))
+const DashboardLazy = lazy(() => import('./components/DashboardApp'))
 
 export default () => {
   const [isSignIn, setIsSignIn] = useState(false)
 
+  useEffect(() => {
+    if (isSignIn) {
+      history.push('/dashboard')
+    }
+  }, [isSignIn])
+
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <StylesProvider generateClassName={generateClassName}>
           <div>
             <Header isSignIn={isSignIn} onSignOut={() => setIsSignIn(false)}/>
@@ -24,11 +34,15 @@ export default () => {
                 <Route path="/auth" component={AuthLazy}> 
                   <AuthLazy onSignIn={() => setIsSignIn(true)}/>
                 </Route>
+                <Route path="/dashboard">
+                  {!isSignIn && <Redirect to='/'/>}
+                  <DashboardLazy />
+                </Route>
                 <Route path="/" component={MarketingLazy} />
               </Switch>
             </Suspense>
           </div>
       </StylesProvider>
-    </BrowserRouter>
+    </Router>
   )
 }
---
+++ mfp/packages/dashboard/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Switch, Route, Router } from 'react-router-dom'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'au',
+})
+
+import Signin from './components/Signin'
+import Signup from './components/Signup'
+
+export default ({ history, onSignIn }) => {
+  return (
+    <div>
+      <StylesProvider generateClassName={generateClassName}>
+        <Router history={history}>
+          <Switch>
+            <Route path="/auth/signin">
+              <Signin onSignIn={onSignIn}/>
+            </Route>
+            <Route path="/auth/signup">
+              <Signup onSignIn={onSignIn}/>
+            </Route>
+          </Switch>
+        </Router>
+      </StylesProvider>
+    </div>
+  )
+}
